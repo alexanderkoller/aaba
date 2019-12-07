183d3e2cd32086f7aa89cea2c16eaf1bb3ebe49f
Alexander Koller <koller@ling.uni-potsdam.de>
2019-12-07 11:53:25 +0100

    fixed testˆ

--- src/test/java/de/saar/coli/arranger/ConfigTest.java
+++ src/test/java/de/saar/coli/arranger/ConfigTest.java
@@ -3,20 +3,16 @@ package de.saar.coli.arranger;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
 public class ConfigTest {
     @Test
-    public void testConfigReader() {
-        InputStream is = getClass().getResourceAsStream("/config.yaml");
-        assertNotNull(is);
-
-        Reader r = new InputStreamReader(is);
-        Config config = Config.read(r);
-
-        assertEquals("Automatically arranged by Alexander's Arranger", config.getArranger());
+    public void testConfigReader() throws FileNotFoundException {
+        Config config = Arrange.loadConfig(null);
+        assertEquals("Automatically arranged by AABA", config.getArranger());
         assertEquals(-20, config.getScores().getHarmonyLeaps());
     }
 }
