df44cb93e13b0491486ea4bdf5d4aab68a66a3ac
Alexander Koller <akoller@gmail.com>
2021-10-21 12:59:01 +0200

    rewriting ABC parser

--- src/main/antlr/AbcNotationParser.g4
+++ src/main/antlr/AbcNotationParser.g4
@@ -23,7 +23,7 @@ endOfLine: NEWLINE | COMMENT;
 // --->TUNE:
 tune: header (voice)+ ;
 // voiceInfo example: V: left hand
-voice: (voiceInfo? score+);
+voice: (voiceInfo score*);
 voiceInfo: VoiceSymbol text=STRING STRING_MODE_EXIT;
 score:(bar suppresScoreLinebreak?)+ (NEWLINE | EOF);
 // >---END OF TUNE
--- src/main/java/de/saar/coli/arranger/abc/AbcParser.java
+++ src/main/java/de/saar/coli/arranger/abc/AbcParser.java
@@ -1,22 +1,25 @@
 package de.saar.coli.arranger.abc;
 
-import de.saar.coli.arranger.Chord;
-import de.saar.coli.arranger.Key;
-import de.saar.coli.arranger.Note;
-import de.saar.coli.arranger.Score;
+import de.saar.coli.arranger.*;
 import de.saar.coli.arranger.abc.AbcNotationLexer;
 import de.saar.coli.arranger.abc.AbcNotationParser;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.antlr.v4.runtime.tree.Trees;
+import org.antlr.v4.runtime.tree.xpath.XPath;
 
 import javax.annotation.processing.Filer;
 import java.io.*;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static de.saar.coli.arranger.Arrange.loadConfig;
 
 /**
  * Reads a score in ABC notation and returns it as a
@@ -35,6 +38,17 @@ import java.util.regex.Pattern;
  * </ul>
  */
 public class AbcParser {
+    public Score readA(Reader abcReader) throws IOException, AbcParsingException {
+        AbcNotationLexer lexer = new AbcNotationLexer(CharStreams.fromReader(abcReader));
+        AbcNotationParser parser = new AbcNotationParser(new CommonTokenStream(lexer));
+
+        AbcParseTreeVisitor visitor = new AbcParseTreeVisitor();
+        ParseTreeWalker.DEFAULT.walk(visitor, parser.tune());
+        return visitor.score;
+    }
+
+
+
     private static Pattern LINE_PATTERN = Pattern.compile("\\s*(\\S+):\\s*(.+)");
 
     /**
@@ -205,17 +219,30 @@ public class AbcParser {
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        AbcNotationLexer lexer = new AbcNotationLexer(CharStreams.fromFileName("issue7-nopiano.abc"));
-        for(Token tok : lexer.getAllTokens()) {
-            System.err.printf("[%s] %s\n", lexer.getVocabulary().getSymbolicName(tok.getType()), tok);
-        }
+    public static void main(String[] args) throws IOException, AbcParsingException {
+        printParseTree("issue7-orig.abc");
+        Score s = new AbcParser().readA(new FileReader("issue7-orig.abc"));
+//        System.err.println(s);
+        Config config = loadConfig("aaba.yaml");
+        new AbcWriter(config).write(s, new FileWriter("x.abc"));
 
-//        System.err.println(lexer.getAllTokens());
-        lexer = new AbcNotationLexer(CharStreams.fromFileName("issue7-nopiano.abc"));
+
+//        AbcNotationLexer lexer = new AbcNotationLexer(CharStreams.fromFileName("issue7-orig.abc"));
+////        for(Token tok : lexer.getAllTokens()) {
+////            System.err.printf("[%s] %s\n", lexer.getVocabulary().getSymbolicName(tok.getType()), tok);
+////        }
+//
+////        System.err.println(lexer.getAllTokens());
+////        lexer = new AbcNotationLexer(CharStreams.fromFileName("issue7-orig.abc"));
+//        AbcNotationParser parser = new AbcNotationParser(new CommonTokenStream(lexer));
+//        System.err.println(printSyntaxTree(parser, parser.tune()));
+////        System.err.println(parser.tune());
+    }
+
+    private static void printParseTree(String filename) throws IOException {
+        AbcNotationLexer lexer = new AbcNotationLexer(CharStreams.fromFileName(filename));
         AbcNotationParser parser = new AbcNotationParser(new CommonTokenStream(lexer));
         System.err.println(printSyntaxTree(parser, parser.tune()));
-//        System.err.println(parser.tune());
     }
 
 
@@ -239,4 +266,150 @@ public class AbcParser {
             }
         }
     }
+
+    private static class AbcParseTreeVisitor extends de.saar.coli.arranger.abc.AbcNotationParserBaseListener {
+        private Score score = new Score("", "", "", 4);
+        private int timeInEighths = 0;
+        private Key key = Key.lookup("C");
+        private List<Note> leadPart = null;
+        private int defaultNoteLength = 0; // in eighths
+
+        // TODO: read words - also need to implement this in grammar
+
+        @Override
+        public void enterVoiceInfo(AbcNotationParser.VoiceInfoContext ctx) {
+            leadPart = null;
+        }
+
+        @Override
+        public void exitVoiceInfo(AbcNotationParser.VoiceInfoContext ctx) {
+            System.err.println(ctx.STRING().getText());
+            leadPart = score.getPart(1);
+        }
+
+        @Override
+        public void enterTitle(AbcNotationParser.TitleContext ctx) {
+            String title = ctx.string().getText();
+            System.err.printf("TITLE: %s\n", title);
+            score.setTitle(title);
+        }
+
+        @Override
+        public void enterMeter(AbcNotationParser.MeterContext ctx) {
+            int numerator = Integer.parseInt(ctx.fraction().numerator.getText());
+            int denominator = Integer.parseInt(ctx.fraction().denominator.getText());
+
+            int numeratorInQuarters = (numerator*4)/denominator; // numerator of the */4 time signature that is equivalent to n/d
+            score.setQuartersPerMeasure(numeratorInQuarters);
+
+            if( defaultNoteLength == 0 ) {
+                // If no L: is given, it is determined from M:
+                // See here: https://abcnotation.com/wiki/abc:standard:v2.1#lunit_note_length
+                if( ((double) numerator) / denominator < 0.75 ) {
+                    setDefaultNoteLength(1, 16);
+                } else {
+                    setDefaultNoteLength(1, 8);
+                }
+            }
+        }
+
+        private void setDefaultNoteLength(int numerator, int denominator) {
+            int eighthsInDenominator = 8/denominator; // hopefully the denominator is a divisor of 8
+            defaultNoteLength = numerator * eighthsInDenominator;
+        }
+
+        @Override
+        public void enterLength(AbcNotationParser.LengthContext ctx) {
+            int numerator = Integer.parseInt(ctx.fraction().numerator.getText());
+            int denominator = Integer.parseInt(ctx.fraction().denominator.getText());
+            setDefaultNoteLength(numerator, denominator);
+        }
+
+        @Override
+        public void enterKey(AbcNotationParser.KeyContext ctx) {
+            String keyString = ctx.string().getText().trim();
+            score.setKey(keyString);
+            key = Key.lookup(keyString);
+            System.err.printf("KEY: '%s'\n", key);
+        }
+
+        @Override
+        public void enterComposer(AbcNotationParser.ComposerContext ctx) {
+            String composer = ctx.string().getText();
+            score.setComposer(composer);
+        }
+
+        @Override
+        public void enterTempo(AbcNotationParser.TempoContext ctx) {
+            // TODO: score.setTempo(value);
+            super.enterTempo(ctx);
+        }
+
+        @Override
+        public void enterNote(AbcNotationParser.NoteContext ctx) {
+            if( leadPart != null ) {
+                String noteStr = ctx.noteExpression().noteString.getText();
+
+                // interpret note in current key
+                String relativeNote = Character.toString(noteStr.charAt(0));
+                int accidentalOffset = key.getAccidentalForNote(relativeNote);
+
+                // explicit accidentals overwrite the accidental offset
+                if (ctx.accidental() != null) {
+                    if (ctx.accidental().sharp() != null) {
+                        accidentalOffset = +1;
+                    } else if (ctx.accidental().flat() != null) {
+                        accidentalOffset = -1;
+                    } else if (ctx.accidental().natural() != null) {
+                        accidentalOffset = 0;
+                    }
+                }
+
+                // move note to correct octave
+                int octave = Character.isUpperCase(relativeNote.charAt(0)) ? 4 : 5;
+
+                if( ctx.noteOctave() != null ) {
+                    if (ctx.noteOctave().octaveUp() != null) {
+                        octave += ctx.noteOctave().octaveUp().size();
+                    }
+
+                    if (ctx.noteOctave().octaveDown() != null) {
+                        octave -= ctx.noteOctave().octaveDown().size();
+                    }
+                }
+
+                // parse duration
+                int duration = defaultNoteLength;
+                if (ctx.noteLength() != null) {
+                    duration = Integer.parseInt(ctx.noteLength().multiplier().getText());
+                }
+
+                Note note = Note.create(relativeNote.toUpperCase(), octave, duration);
+                note = note.add(accidentalOffset);
+
+                leadPart.add(note);
+                timeInEighths += duration;
+            }
+        }
+
+        @Override
+        public void enterChord(AbcNotationParser.ChordContext ctx) {
+            if( leadPart != null ) {  // skip spurious "chords" from the header
+                String chordStr = ctx.STRING().getText();
+                Chord chord = Chord.lookup(chordStr);
+
+                if (chord == null) {
+                    System.err.printf("Warning: unknown chord '%s' at time %d.\n", chordStr, timeInEighths);
+                } else {
+                    score.addChord(timeInEighths, chord);
+                }
+            }
+        }
+
+        @Override
+        public void enterRest(AbcNotationParser.RestContext ctx) {
+            System.err.println("rest");
+        }
+    }
 }
+
